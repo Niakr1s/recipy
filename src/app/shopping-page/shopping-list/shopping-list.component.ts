9f367f7c68e1bc8cdca106306f434691838ee4f8
Niakr1s <pavel2188@gmail.com>
2021-02-18 15:46:44 +0300

    shopping-list.component correctly takes data from recipes.service

--- src/app/shopping-page/shopping-list/shopping-list.component.ts
+++ src/app/shopping-page/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipesService } from 'src/app/services/recipes.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
-  constructor() { }
+  constructor(readonly recipesService: RecipesService) { }
 
   ngOnInit(): void {
   }
