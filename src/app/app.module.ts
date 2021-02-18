b9b54526690c0beedddc8742e203a6cfe6673d37
Niakr1s <pavel2188@gmail.com>
2021-02-18 11:50:05 +0300

    added recipes-page and shopping-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-page/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-page/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipes-page/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes-page/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes-page/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes-page/recipes-page.component';
+import { ShoppingComponent } from './shopping-page/shopping-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipesComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes-page/recipes-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-page',
+  templateUrl: './recipes-page.component.html',
+  styleUrls: ['./recipes-page.component.css']
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
